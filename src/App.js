b300896112696d1661f37a7d2d9d33d330568053
mzncvmc <mzncvmc@gmail.com>
2022-12-25 16:43:58 +0900

    #10.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-const Food = ({fav}) => {
-  return <h1>I like {fav}</h1>;
-}
+const Food = ({name, picture}) => {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src = {picture} />
+    </div>
+  );
+};
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://kelliesfoodtoglow.com/wp-content/uploads/2016/10/kimchi1.jpg'
+  },
+  {
+    name: 'kimbop',
+    image: 'https://chefjulieyoon.com/wp-content/uploads/2011/12/Kimbop-4-1.jpg?x80339'
+  },
+  {
+    name: 'bibimbop',
+    image: 'https://www.escoffier.edu/wp-content/uploads/2016/06/ThinkstockPhotos-532125694.jpg'
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Hello~~!!!</h1>
-      <Food fav = "kimchi" />
-      <Food fav = "ramen" />
-      <Food fav = "chukumi" />
+      {foodILike.map(dish => <Food name = {dish.name} picture = {dish.image} />)}
     </div>
   );
 }
