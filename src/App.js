a1f6a41cb3867e84d016aba2ad430e0eeedb1dff
mzncvmc <mzncvmc@gmail.com>
2022-12-25 16:18:57 +0900

    #10.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Potato from './Potato';
+
+const Food = ({fav}) => {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello~~!!!</h1>
-      <Potato />
+      <Food fav = "kimchi" />
+      <Food fav = "ramen" />
+      <Food fav = "chukumi" />
     </div>
   );
 }
